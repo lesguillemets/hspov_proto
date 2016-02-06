c7ac36458ce95a8b54bf0447cf83fd142d6d65cd
lesguillemets <lesguillemets@users.noreply.github.com>
2016-02-06 19:15:27 +0900

    update example

--- examples/Example.hs
+++ examples/Example.hs
@@ -1,4 +1,12 @@
 import Data.Povray
+import Data.Povray.Base
+import Data.Povray.Types
+import Data.Povray.Objects
+import Data.Povray.ObjectModifiers
+import Data.Povray.Texture
+import Data.Povray.Transformation
 
 main = do
     include "colors.inc"
+    putStrLn . toPov $ Box (V 0 2 3) (V 3 5 10)  $
+                    OModify Nothing (Just $ Transformation "f") (Just "COMMO")
