5fe6c6104134ec8bb34a19d95cf14073b632896f
lesguillemets <lesguillemets@users.noreply.github.com>
2016-02-09 19:37:15 +0900

    separate pigment and texture

--- src/Data/Povray/Texture.hs
+++ src/Data/Povray/Texture.hs
@@ -1,40 +1,11 @@
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE DisambiguateRecordFields #-}
 module Data.Povray.Texture where
 
 import Data.Povray.Base
 import Data.Povray.Colour
+import Data.Povray.Pigment
 import Data.Povray.Types
 
 import Data.Maybe
-
-data Pigment = Pigment {
-    _color :: Maybe Colour,
-    _named :: Maybe NamedPigment
-}
-
-emptyPigment :: Pigment
-emptyPigment = Pigment Nothing Nothing
-
-instance Povray Pigment where
-    toPov (p@Pigment{..}) =
-        case join [
-                maybeToPov _color,
-                maybeToPov _named
-                ] of
-                    "" -> ""
-                    s -> join ["pigment {", s, "}"]
-
--- TODO : This is too ad hoc
-data NamedPigment = Checker (Maybe Colour) (Maybe Colour)
-                  | Named Str
-instance Povray NamedPigment where
-    toPov (Checker c0 c1) =
-        join [ "checker",
-               maybeToPov c0,
-               maybeToPov c1
-            ]
-    toPov (Named s) = s
 
 
 data Finish = Phong Double
