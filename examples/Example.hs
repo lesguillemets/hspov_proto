c6b2b4af54e3d799ece14bdb670c14f1ffcfbb89
lesguillemets <lesguillemets@users.noreply.github.com>
2016-02-09 18:43:01 +0900

    update example

--- examples/Example.hs
+++ examples/Example.hs
@@ -1,12 +1,52 @@
 import Data.Povray
 import Data.Povray.Base
 import Data.Povray.Types
+import Data.Povray.Camera
+import Data.Povray.Colour
+import Data.Povray.Light
 import Data.Povray.Objects
 import Data.Povray.ObjectModifiers
 import Data.Povray.Texture
 import Data.Povray.Transformation
 
+o :: Vect
+o = V 0 0 0
+ecolor :: Colour
+ecolor = RGB (V 0 0.8 1.5)
+white = RGB (V 1 1 1)
+black = RGB (V 0 0 0)
+
+epig :: Pigment
+epig = emptyPigment {_color = Just ecolor}
+eglass :: Pigment
+eglass = emptyPigment {_named = Just (Named "M_Glass3")}
+echeck :: Pigment
+echeck = emptyPigment {_named = Just (Checker (Just white) (Just black))}
+
+etexture :: Texture
+etexture = Texture (Just epig) (Just (Phong 0.5))
+box :: Object
+box = Box o (V 1 1 1) (emptyModifier {_texture = Just etexture})
+
+
+efloorTexture :: Texture
+efloorTexture = Texture (Just echeck) (Just (Phong 0.1))
+efloor :: Object
+efloor = Plane (V 0 1 0) 0 (emptyModifier {_texture = Just efloorTexture})
+
+camera :: Camera
+camera = Camera (V 2 9 10) o
+
+lights :: [Light]
+lights = [
+    Light (V 100 100 (-100)) (RGB (V 1.2 1.2 1.2)),
+    Light (V 0 10 (-120)) (RGB (V 0.3 0.3 0.3))
+    ]
+          
 main = do
     include "colors.inc"
-    putStrLn . toPov $ Box (V 0 2 3) (V 3 5 10)  $
-                    OModify Nothing (Just $ Translate (V 0 0 2)) (Just "COMMO")
+    include "textures.inc"
+    put camera
+    put efloor
+    put box
+    mapM_ put lights
